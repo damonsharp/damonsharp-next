5b8b616c6446450bc6cdfb0180c14c4594218ea2
Damon Sharp <damon.sharp@gmail.com>
2025-11-01 19:45:17 -0400

    add honeypot to contact form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -4,7 +4,10 @@ import { sendEmail } from "@/actions/sendEmail";
 import { useActionState } from "react";
 import { Button } from "@/components/ui/button";
 import { contactFormInitialState } from "@/lib/validators";
-import { faArrowAltCircleRight, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+	faArrowAltCircleRight,
+	faCheckSquare
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ContactForm = ({ className }) => {
@@ -12,9 +15,9 @@ const ContactForm = ({ className }) => {
 
 	return (
 		<>
-			{formState.formMessage ? (
+			{formState?.formMessage ? (
 				<div className="flex items-center p-6 border-4 border-success rounded text-success text-lg">
-					<FontAwesomeIcon icon={faCheckSquare} className="text-4xl" />
+					<FontAwesomeIcon icon={faCheckSquare} className="text-4xl"/>
 					{formState.formMessage}
 				</div>
 			) : (
@@ -24,9 +27,9 @@ const ContactForm = ({ className }) => {
 							<label className="block" htmlFor="fullName">
 								Full Name
 							</label>
-							{formState.errors.fieldErrors?.fullName && (
+							{formState?.errors?.fieldErrors?.fullName && (
 								<p className="text-destructive my-2 text-sm">
-									{formState.errors.fieldErrors.fullName[0]}
+									{formState?.errors.fieldErrors.fullName[ 0 ]}
 								</p>
 							)}
 							<input
@@ -34,7 +37,7 @@ const ContactForm = ({ className }) => {
 								type="text"
 								id="fullName"
 								name="fullName"
-								defaultValue={formState.fullName}
+								defaultValue={formState?.fullName}
 								placeholder="What is your full name?"
 								max={80}
 								required
@@ -44,15 +47,15 @@ const ContactForm = ({ className }) => {
 							<label className="block" htmlFor="email">
 								Email Address
 							</label>
-							{formState.errors.fieldErrors?.email && (
-								<p className="text-destructive my-2 text-sm">{formState.errors.fieldErrors.email[0]}</p>
+							{formState?.errors?.fieldErrors?.email && (
+								<p className="text-destructive my-2 text-sm">{formState?.errors.fieldErrors.email[ 0 ]}</p>
 							)}
 							<input
 								className="p-3 w-full rounded-xs border border-accent"
 								type="email"
 								id="email"
 								name="email"
-								defaultValue={formState.email}
+								defaultValue={formState?.email}
 								placeholder="What is your email address?"
 								max={80}
 								required
@@ -63,23 +66,33 @@ const ContactForm = ({ className }) => {
 						<label className="mt-5 block" htmlFor="message">
 							Message
 						</label>
-						{formState.errors.fieldErrors?.message && (
-							<p className="text-destructive my-2 text-sm">{formState.errors.fieldErrors.message[0]}</p>
+						{formState?.errors?.fieldErrors?.message && (
+							<p className="text-destructive my-2 text-sm">{formState?.errors.fieldErrors.message[ 0 ]}</p>
 						)}
 						<textarea
 							className="p-3 mt-1 rounded-xs w-full h-64 border border-accent"
 							id="message"
 							name="message"
-							defaultValue={formState.message}
+							defaultValue={formState?.message}
 							placeholder="What would you like to say?"
 							required
+						/>
+					</div>
+					<div className="hidden">
+						<input
+							type="hidden"
+							name="company"
+							defaultValue=""
+							max={80}
+							tabIndex="-1"
+							autoComplete="one-time-code"
 						/>
 					</div>
 					<Button
 						className="bg-primary text-neutral hover:bg-accent hover:text-primary px-5 py-4 rounded mt-5 cursor-pointer"
 						disabled={isPending}
 					>
-						{isPending ? "Sending" : "Send Message"} <FontAwesomeIcon icon={faArrowAltCircleRight} />
+						{isPending ? "Sending" : "Send Message"} <FontAwesomeIcon icon={faArrowAltCircleRight}/>
 					</Button>
 				</form>
 			)}
