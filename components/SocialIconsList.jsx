993630e14f721ac257c5a93ceddc32e67ad58c3b
Damon Sharp <damon.sharp@gmail.com>
2025-11-03 16:35:40 -0500

    swap social icons order

--- components/SocialIconsList.jsx
+++ components/SocialIconsList.jsx
@@ -11,16 +11,16 @@ import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
 const SocialIconsList = () => {
 	return (
 		<div className="flex gap-3 col-span-1 items-center">
+			<SocialIcon link="/contact" icon={faSquareEnvelope}
+			            className="size-6 text-2xl text-neutral hover:text-accent" label="Email icon"/>
 			<SocialIcon link="https://www.linkedin.com/in/damonsharp" icon={faLinkedin}
 			            className="size-6 text-2xl text-neutral hover:text-accent" label="LinkedIn icon"/>
 			<SocialIcon link="https://github.com/damonsharp" icon={faGithub}
 			            className="size-6 text-2xl text-neutral hover:text-accent" label="GitHub icon"/>
-			<SocialIcon link="https://phpc.social/@damonsharp" icon={faMastodon}
-			            className="size-6 text-2xl text-neutral hover:text-accent" label="Mastodon icon"/>
-			<SocialIcon link="/contact" icon={faSquareEnvelope}
-			            className="size-6 text-2xl text-neutral hover:text-accent" label="Email icon"/>
 			<SocialIcon link="https://x.com/damonsharp" icon={faXTwitter}
 			            className="size-6 text-2xl text-neutral hover:text-accent" label="Twitter (X) icon"/>
+			<SocialIcon link="https://phpc.social/@damonsharp" icon={faMastodon}
+			            className="size-6 text-2xl text-neutral hover:text-accent" label="Mastodon icon"/>
 		</div>
 	);
 };
