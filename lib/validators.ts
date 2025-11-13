958848e8350fa42cb5638bc59fe0d68aaa2e7b94
Damon Sharp <damon.sharp@gmail.com>
2025-11-13 17:47:01 -0500

    revert last commit

--- lib/validators.ts
+++ lib/validators.ts
@@ -4,7 +4,7 @@ export const contactFormSchema = z.object({
 	fullName: z.string().min(3, "It would be helpful to have your name."),
 	email: z.email("I need a valid email address in order to reply to you."),
 	message: z.string().min(10, "What would you like to say?"),
-	juststop: z.union([z.literal("6"), z.literal("six")], "Please try again"),
+	juststop: z.union([z.literal("6"), z.literal("six"), z.literal("Six")], "Please try again"),
 	errors: z.object().optional(),
 	formMessage: z.string().optional()
 });
